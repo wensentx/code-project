3ff776418860b278b5659247f87a604187122cc0
wensentx <9anvi9@gmail.com>
2024-09-22 02:12:01 +0300

    Создание баз данных модерации

--- cogs/femida/database/connection.py
+++ cogs/femida/database/connection.py
@@ -28,6 +28,6 @@ async def get_session() -> AsyncSession:
 
 async def create_tables():
     async with engine.begin() as conn:
-        # pass
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+        pass
+        # await conn.run_sync(Base.metadata.drop_all)
+        # await conn.run_sync(Base.metadata.create_all)
